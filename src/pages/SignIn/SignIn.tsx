4cbc40a7c247b60719348f5bf9123fd1fea73f34
CrisSSD <cdcalderonrojas@gmail.com>
2025-02-16 22:37:44 +0100

    Iniciamos con SignIn

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { IonPage, IonContent, IonButton, IonText } from '@ionic/react';
+import { IonPage, IonContent, IonButton, IonText, IonImg } from '@ionic/react';
+
+import lucas from '../../assets/lucas.jpg';
 
 const SignIn: React.FC = () => {
   const history = useHistory();
@@ -8,7 +10,22 @@ const SignIn: React.FC = () => {
   return (
     <IonPage>
       <IonContent className="ion-padding">
-       
+        {/* Imagen */}
+        <div className="perfil">
+          <div className="image-container">
+            <IonImg src={lucas} alt="Logo de la App" className="logo-img" />
+          </div>
+
+          {/* Texto */}
+          <div className="text-container">
+            <IonText>
+              <p className="sign-in-title">User</p>
+              <p className="sign-in-description">
+                Sumario
+              </p>
+            </IonText>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
